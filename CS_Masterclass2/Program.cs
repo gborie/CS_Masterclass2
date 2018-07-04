e93d70f028864f6e7e5aefa1cca79b749630db3f
gborie <gborie7@gmail.com>
2018-07-04 15:27:06 +0100

    try catch and finally challenge - Divide by zero

--- CS_Masterclass2/Program.cs
+++ CS_Masterclass2/Program.cs
@@ -11,29 +11,20 @@ namespace CS_Masterclass2
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number!");
-            string userInput = Console.ReadLine();
+            int num1 = 10;
+            int num2 = 0;
+
+            int result;
 
             try
             {
-                int userInputAsInt = int.Parse(userInput);
+                result = num1 / num2;
+
             }
-            catch (FormatException)
+            catch (Exception)
             {
 
-                Console.WriteLine("Format exception, please enter the correct type next time.");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Overflow exception, the number was too long or too short for an int32");
-            }
-            catch (ArgumentNullException)
-            {
-                Console.WriteLine("ArgumentNullException, the value was empty");
-            }
-            finally
-            {
-                Console.WriteLine("This is called anyways!");
+                Console.WriteLine("Can't divide by zero!");
             }
 
             Console.ReadKey();
