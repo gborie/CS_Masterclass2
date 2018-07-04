ac45352757df96998f28d2524b9dffd08f3d1997
gborie <gborie7@gmail.com>
2018-07-04 10:37:39 +0100

    void methods created

--- CS_Masterclass2/Program.cs
+++ CS_Masterclass2/Program.cs
@@ -10,6 +10,20 @@ namespace CS_Masterclass2
     {
         static void Main(string[] args)
         {
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from a method");
+            Console.Read();
         }
+
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+
     }
 }
